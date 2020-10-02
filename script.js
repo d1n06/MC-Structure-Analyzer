bf535ba17b933e24094556f6624dd5d6de8fb239
Diegox6 <diegolaborda@gmail.com>
2020-10-03 01:59:06 +0200

    palette finding optimization

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let data = [];
 
+let start, end;
+
 function analyze() {
 	resetDisplay();
+
+	let start = new Date().getTime();
+
 	let file = document.getElementById("filein").files[0];
 	let fr = new FileReader();
 	fr.readAsArrayBuffer(file);
@@ -9,14 +14,14 @@ function analyze() {
 		data = Array.from(new Int8Array(e.target.result));
 		document.getElementById("data").style.display = "block";
 		hexDump(data);
-		
+
 		let size = getSize();
-		
+
 		let palette = getPalette();
 		let blockCount = getBlockCount(palette.length, size[0]*size[1]*size[2]);
-		
+
 		let repeated = []
-		
+
 		for (i = 0; i < palette.length; i++)
 			for (j = i+1; j < palette.length; j++)
 				if (palette[i] == palette[j] && !repeated.includes(i)) {
@@ -24,24 +29,27 @@ function analyze() {
 					blockCount[i] += blockCount[j];
 					repeated.push(j);
 				}
-		
+
 		palette = [...new Set(palette)];
-		
+
 		let newBlockCount = []
 		for (i = 0; i < blockCount.length; i++) if (!repeated.includes(i)) newBlockCount.push(blockCount[i]);
-		
+
 		document.getElementById("properties").style.display = "block";
-		
+
 		document.getElementById("sizeHolder").innerHTML = size[0] + "x" + size[1] + "x" + size[2] + "<br>";
-		
+
 		for (let i = 0; i < palette.length; i++) document.getElementById("blocksHolder").innerHTML += "<li>" + newBlockCount[i] + " x " + palette[i] + "</li>";
+
+		end = new Date().getTime();
+		console.log(end - start);
 	}
 }
 
 function getSize() {
 	let size = [0, 0, 0];
 	let ptr = null;
-	
+
 	let target = "size";
 	for (let i = 0; i < data.length; i++) {
 		let score = 0;
@@ -57,7 +65,7 @@ function getSize() {
 		console.log("error, couldn't find size!");
 		return;
 	}
-	
+
 	for (let i = 0; i < 4; i++) {
 		size[0] += data[ptr+9+i]*Math.pow(16,3-i);
 		size[1] += data[ptr+13+i]*Math.pow(16,3-i);
@@ -69,15 +77,15 @@ function getSize() {
 function getPalette() {
 	let palette = [];
 	let len = null;
-	
+
 	let ptr = null;
-	
+
 	let target = "palette";
 	for (let i = 0; i < data.length; i++) {
 		let score = 0;
 		for (let j = 0; j < target.length; j++)
 			if (data[i+j] == target.charCodeAt(j)) score++;
-		
+
 		if (score == target.length) {
 			ptr = i;
 			break;
@@ -87,56 +95,51 @@ function getPalette() {
 		console.log("error, couldn't find palette!");
 		return;
 	}
-	
+
 	for (let i = 0; i < 4; i++) len += data[ptr+8+i]*Math.pow(16,3-i);
-	
-	target = "Name";
+
 	for (let n = 0; n < len; n++) {
 		for (let i = ptr; i < data.length; i++) {
-			let score = 0;
-			for (let j = 0; j < target.length; j++)
-				if (data[i+j] == target.charCodeAt(j)) score++;
-			
-			if (score == target.length) {
+			if (data[i] == "N".charCodeAt(0)) {
 				ptr = i;
 				break;
 			}
 		}
-		
+
 		ptr += 4;
 		nameLength = data[ptr]*16+data[ptr+1];
 		palette.push("");
 		for (let j = 0; j < nameLength; j++) palette[n] += String.fromCharCode(data[ptr+2+j]);
-	}	
-	
+	}
+
 	return palette;
 }
 
 function getBlockCount(states, blocks) {
 	let blockCount = [];
 	for (i = 0; i < states; i++) blockCount.push(0);
-	
-	let ptr = 0;
+
+	let ptr = 84;
 	for (n = 0; n < blocks; n++) {
 		let target = "state";
 		for (let i = ptr; i < data.length; i++) {
 			let score = 0;
 			for (let j = 0; j < target.length; j++)
 				if (data[i+j] == target.charCodeAt(j)) score++;
-			
+
 			if (score == target.length) {
 				ptr = i;
 				break;
 			}
 		}
-		
+
 		let state = 0;
 		for (let i = 0; i < 4; i++) state += data[ptr+5+i]*Math.pow(16,3-i);
 		blockCount[state]++;
-		
+
 		ptr += 8;
 	}
-	
+
 	return blockCount;
 }
 
@@ -148,14 +151,14 @@ function hex(n, d, prefix=true) {
 
 function hexDump(data) {
 	for (let i = 0; i < data.length%12; i++) data.push(0);
-		
+
 	for (let i = 0; i < data.length / 12; i++) {
 		let line = hex(i*12,4) + ": ";
-		
+
 		for (let j = 0; j < 12; j++) line += hex(data[i*12+j],2,false) + " ";
-		
+
 		line += "| ";
-		
+
 		for (let j = 0; j < 12; j++) {
 			let c = data[i*12+j];
 			if ((c >= 97 && c <= 122) || (c >= 48 && c <= 57) || (c >= 65 && c <= 90))
@@ -163,7 +166,7 @@ function hexDump(data) {
 			else
 				line += ".";
 		}
-		
+
 		line += "<br>";
 		document.getElementById("dataHolder").innerHTML += line;
 	}
